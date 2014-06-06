f90718d6eeecd80783056db4076ab077618ed398
Monika <Monika.Zillinger@gmail.com>
2014-06-06 10:06:39 -0700

    Added a comment

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,7 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //this is a test for git
 //Look we changed a line
+//Look I added a comment
 
 #include "stdafx.h"
 
