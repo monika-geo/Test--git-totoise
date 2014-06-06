a16aae8b2de2b6264cd211fb1cfcc304f3132454
Monika <Monika.Zillinger@gmail.com>
2014-06-06 09:56:20 -0700

    changed a line

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//this is a test for git
+//Look we changed a line
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
